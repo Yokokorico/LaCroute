c420db77a083d09a2d42fee85cf8b80b0841332c
Théo Duflos <theo.duflos.yokoko@gmail.com>
2023-12-27 14:09:08 +0100

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LaCroute.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
