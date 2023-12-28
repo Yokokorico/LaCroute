6cc51d682e775e691c179f31c468769dbc3cf047
MorganeMjk <morgane.mjk@outlook.fr>
2023-12-28 15:57:30 +0100

    feat: :sparkles: add menu

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using LaCroute.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaCroute.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly ILogger<MenuController> _logger;
+
+        public MenuController(ILogger<MenuController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
