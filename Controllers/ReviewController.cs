a8087e310a754b233f8dc08c9100fea23bd19eee
Merlinovitch <a.merlin92@gmail.com>
2024-01-02 11:50:40 +0100

    feat; map

---
+++ Controllers/ReviewController.cs
@@ -0,0 +1,23 @@
+﻿/*using LaCroute.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaCroute.Controllers
+{
+	public class ReviewController : Controller
+	{
+		private readonly LaCrouteContext _context;
+
+		public ReviewController(LaCrouteContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var reviews = await _context.Review.OrderByDescending(reviews => reviews.created_at).Take(5).ToListAsync();
+			return View(reviews);
+		}
+	}
+}
+*/
---
+++ Migrations/20231229135325_ReviewsCreate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LaCroute.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReviewsCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "update_at",
+                table: "Review",
+                newName: "updated_at");
+
+            migrationBuilder.RenameColumn(
+                name: "create_at",
+                table: "Review",
+                newName: "created_at");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Review",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Review");
+
+            migrationBuilder.RenameColumn(
+                name: "updated_at",
+                table: "Review",
+                newName: "update_at");
+
+            migrationBuilder.RenameColumn(
+                name: "created_at",
+                table: "Review",
+                newName: "create_at");
+        }
+    }
+}
